60e6a17feb4d03a384077d69c1eaf62d8d65f9f7
Laboratório FIAP <brunobiletsky@gmail.com>
2024-09-26 09:08:34 -0300

    arrumando as rotas e excluindo route a mais (cadastrar sera feito em clientes)

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Erro from './routes/Erro.jsx'
+import Jogo from './routes/Jogo.jsx'
+import Login from './routes/Login.jsx'
+import Home from './routes/Home.jsx'
+import Clientes from './routes/Clientes.jsx'
 
 const router = createBrowserRouter([{
   path:'/', element: <App/>,
@@ -10,7 +15,7 @@ const router = createBrowserRouter([{
   children:[
     {path:'/',element: <Home/>},
     {path:'/clientes',element: <Clientes/>},
-    {path:'/cadastrar',element: <Cadastrar/>},
+    {path:'/login',element: <Login/>},
     {path:'/jogo',element: <Jogo/>}
 
 

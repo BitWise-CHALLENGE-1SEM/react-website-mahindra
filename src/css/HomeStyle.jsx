79d54b526fee7ebb9bc99ae7876f7468135f8f99
Laboratório FIAP <brunobiletsky@gmail.com>
2024-09-26 11:27:37 -0300

    adicionando secao 1 comentada na home (trabalhar nisso dps)

--- src/css/HomeStyle.jsx
+++ src/css/HomeStyle.jsx
@@ -1,4 +1,74 @@
 import styled from "styled-components";
 export const HomeStyle = styled.section`
 
-`
+/* .secao1{
+    padding-top: 150px;
+    position: relative;
+    background-image: radial-gradient( #444444 ,#222222 );
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;  
+    border-bottom: 8px solid #242424b6 ;
+}
+
+.secao1 h3{
+    color: white;
+    padding-bottom: 50px;
+    font-size: 37px;
+    text-transform: uppercase;
+    -webkit-text-stroke-color: black;
+    -webkit-text-stroke-width: 1.5px;
+}
+
+.slider{
+    padding: 0 0 0 410px;
+    width: 100%;
+    height: 620px;
+    overflow: hidden;
+}
+
+.slides{
+    width: 310%;
+    height: 650px;
+    display: flex;
+}
+
+.slides input{
+    display: none;
+}
+
+.slide{
+    width: 25%;
+    position: relative;
+    transition: 2s;
+}
+
+.slide img{
+    width: 93%;
+    height: 80%;
+    border: 8px solid var(--color5);
+}
+
+#radio1:checked ~ .first{
+    margin-left: 0;
+
+}
+
+#radio2:checked ~ .first{
+    margin-left: -25%;
+
+}
+
+#radio3:checked ~ .first{
+    margin-left: -50%;
+
+}
+
+#radio4:checked ~ .first{
+    margin-left: -75%;
+
+}*/
+
+
+` 
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,44 @@
+import gp_sp from "../assets/tracks/gp-sao-paulo.webp";
+import gp_monaco from "../assets/tracks/gp-monaco.webp";
+import gp_berlim from "../assets/tracks/gp-berlim.webp";
+import gp_uer from "../assets/tracks/gp-uer.webp";
+
+
+
 
 const Home=()=>{
     
     return(
         <>
-
+        {/* <section className="secao1">
+            <h3>Inserir as pessoas no mundo da formula-E!</h3>
+            <div className="slider">
+                <div className="slides">
+                    <input type="radio" name="radio-btn" id="radio1"/>
+                    <input type="radio" name="radio-btn" id="radio2"/>
+                    <input type="radio" name="radio-btn" id="radio3"/>
+                    <input type="radio" name="radio-btn" id="radio4"/>
+            
+                    {/* <!-- SLIDES IMAGE --> */}
+                    {/* <div className="slide first">
+                        <img src={gp_sp} alt="imagem 1"/>
+                    </div>
+            
+                    <div className="slide">
+                        <img src={gp_berlim} alt="imagem 2"/>
+                    </div>
+            
+                    <div className="slide">
+                        <img src={gp_monaco} alt="imagem 3"/>
+                    </div>
+            
+                    <div className="slide">
+                        <img src={gp_uer} alt="imagem 4"/>
+                    </div>
+                </div>
+            </div>
+        </section> */} 
+        
         </>
     )
 }

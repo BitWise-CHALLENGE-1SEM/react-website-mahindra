7680cf088f7b1fdbc9e72390be310bd754c2a77a
isabelladutra126 <brunobiletsky@gmail.com>
2024-10-23 13:02:40 -0300

    secao2 finalizada em tecnologias

--- src/css/TecnologiasStyle.jsx
+++ src/css/TecnologiasStyle.jsx
@@ -82,4 +82,87 @@ export const TecnologiasStyle = styled.section`
     }
     
 }
+
+/* secao 2 */
+.btn{
+    padding-bottom: 3vh;
+}
+.btnCorrer{
+    background: var(--color4);
+    width: 120px;
+    height: 60px;
+    border-radius: 2rem;
+    transition: 0.8s;
+    border: 1px solid var(--color2);
+    font-weight: 700;
+
+}
+.btnCorrer:hover{
+    background: var(--color4);
+    color: var(--color1);
+    box-shadow: 5px 5px 4px var(--color5);
+    text-transform: uppercase;
+    
+}
+.secao2{
+    max-width: 100%;
+    display: grid;
+    grid-template-columns: repeat(2,1fr);
+    text-align: center;
+    align-items: center;
+    background-image: radial-gradient(rgb(68, 68, 68), rgb(34, 34, 34));
+} 
+.order-1{
+    order:1;
+}
+.order-2{
+    order:2;
+}
+.secao2-text h1{
+    margin-top: 100px;  
+    line-height: 1.1;
+    color: white;
+    text-transform: uppercase;
+    font-size: 1.7rem;
+}
+.titulo1{
+    font-size: 2.5rem;
+    text-transform: uppercase;
+    color:var(--color4);
+}
+.secao2-text p{
+    margin: 2rem auto 2rem;
+    width: 60%;
+    color: var(--color1);
+    font-size: 1.7rem;
+}
+.secao2-text p a{
+    color: var(--color4);
+}
+.image{
+    margin-top: 100px;
+    width: 80%;
+}
+.secao2-image:hover{
+    cursor: pointer;
+    animation: carro 0.9s ease-in-out;
+}
+@keyframes carro{
+    from{
+        transform: translateX(100px);
+    }
+    to{
+        transform: translateX(10px);
+        filter:drop-shadow(var(--color1) 0 0 2rem)
+    }
+
+}
+.hovered{
+    filter:drop-shadow(var(--color1) 0 0 2rem)
+}
+
+.lista{
+    list-style: circle;
+    color: #dddddd;
+}
 `
--- src/routes/Tecnologias.jsx
+++ src/routes/Tecnologias.jsx
@@ -1,7 +1,20 @@
 import { TecnologiasStyle } from "../css/TecnologiasStyle"
 import Nav from "../components/Nav"
-
+import carro from "../assets/carromahindrapng.webp"
+import acelerando from "../assets/audios/acelerando.mp3"
 const Tecnologias=()=>{
+    function animated(){
+        const carro = document.querySelector('.image');
+        carro.classList.add('hovered');
+        setTimeout(() => {
+            carro.classList.remove('hovered');
+        }, 15000);
+    }
+    function acelerar(){
+        const x = document.querySelector(".musica")
+        x.play()
+    }
+    
     return(
         <>
         <TecnologiasStyle>
@@ -9,18 +22,35 @@ const Tecnologias=()=>{
             <section className="corpo">
                 <section className="secao1">
                     <h1>A Fórmula-E promove inovações tecnológicas.</h1>
-                        <div className="container">
-                            <div className="content">
-                                <h2>A Categoria Exportadora de Tech </h2>
-                                <p>A Fórmula E tem sido um campo de testes incrível para tecnologias que acabam sendo aplicadas em nosso dia a dia. O fato de evoluir o carro na pista permite transportar as tecnologias criadas para os novos carros elétricos das grandes marcas.</p>
-                            </div>  
-                            <div className="content">
-                                <h2>Tecnologias Exclusivas de Ponta </h2>
-                                <p>A Fórmula E é um espetáculo de inovações e tecnologias revolucionárias que nunca foram vistas em outros eventos automobilísticos. A categoria conta com um modelo de qualificação duelo, sistema de Attack Mode, Fanboost e tração integral variável. A implementação dessas tecnologias exclusivas exemplificam o futuro das competições automotivas, estabelecendo novos padrões de engenharia e performance.</p>
-                            </div>
+                    <div className="container">
+                        <div className="content">
+                            <h2>A Categoria Exportadora de Tech </h2>
+                            <p>A Fórmula E tem sido um campo de testes incrível para tecnologias que acabam sendo aplicadas em nosso dia a dia. O fato de evoluir o carro na pista permite transportar as tecnologias criadas para os novos carros elétricos das grandes marcas.</p>
+                        </div>  
+                        <div className="content">
+                            <h2>Tecnologias Exclusivas de Ponta </h2>
+                            <p>A Fórmula E é um espetáculo de inovações e tecnologias revolucionárias que nunca foram vistas em outros eventos automobilísticos. A categoria conta com um modelo de qualificação duelo, sistema de Attack Mode, Fanboost e tração integral variável. A implementação dessas tecnologias exclusivas exemplificam o futuro das competições automotivas, estabelecendo novos padrões de engenharia e performance.</p>
                         </div>
-                    </section>
+                    </div>
                 </section>
+                <section className="secao2">
+                    <div className="secao2-text order-1">
+                        <h1>Encontre todas as</h1>
+                        <h2 className="titulo1">tecnologias mais avançadas.</h2>
+                        <p> <a>- Frenagem Regenerativa:</a> Permitem que os carros recuperem energia durante a frenagem, aumentando a eficiência energética.</p>
+                        <p> <a>- Desempenho e Eficiência:</a> O mais recente carro GEN3 Evo pode acelerar de 0 a 100 km/h em apenas 1,82 segundos, sendo 30% mais rápido que um carro de Fórmula 1 atual que faz em 2,6 segundos.  </p>
+                        <p> <a>- Tecnologia de Baterias:</a> Com uma potência de 350 kW, obteve um aumento de 75% de capacidade energética em relação ao modelo anterior</p>
+                        <div className="btn"><button className="btnCorrer" onClick={acelerar}>Vamos correr</button></div>
+                        </div>
+
+                        <div className="secao2-image order-2">
+                        <audio className="musica">
+                            <source src={acelerando} type="audio/mpeg"/>
+                        </audio>
+                        <img src={carro} className="image" alt="carro" onMouseOver={animated}/>
+                    </div>
+                </section>
+            </section>
         </TecnologiasStyle>
         </>
     )
